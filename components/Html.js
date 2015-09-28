ebd31e551009e9d732af2c2d8cde20d455e9f7b4
meriadec <meriadec.pillet@gmail.com>
2015-09-28 14:15:35 +0200

    chore(html): active css by default for many reasons
    ask me for more infos

--- components/Html.js
+++ components/Html.js
@@ -14,10 +14,7 @@ export default class Html extends React.Component {
           <meta charSet='utf-8' />
           <title>{ pageTitle }</title>
           <meta name='viewport' content='width=device-width, user-scalable=no' />
-          {
-            process.env.NODE_ENV === 'prod' &&
-            <link rel='stylesheet' href='/public/styles.css' />
-          }
+          <link rel='stylesheet' href='/public/styles.css' />
         </head>
         <body>
           <div id='app' dangerouslySetInnerHTML={ { __html: this.props.markup } }></div>
