8d78fb397d851aa604e357eb5926ee43c55f3452
meriadec <meriadec.pillet@gmail.com>
2015-10-06 14:53:28 +0200

    refactor(home): home refactoring

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -2,16 +2,26 @@
 
 import React from 'react';
 
+import { NavLink } from 'fluxible-router';
 import { GithubButton, Loader } from '../ui';
 
 export default class Home extends React.Component {
 
   render () {
     return (
-      <div>
-        Hello. <GithubButton context={this.props.context} />
-        <br />
-        <Loader />
+      <div style={{ padding: '8rem 0', textAlign: 'center' }}>
+        {'KeyCode will come soon.'}<br />
+        {'Be prepared.'}<br /><br />
+        <div className='f fjc'>
+          <NavLink
+            routeName='game'
+            className='ZavButton'>
+            <i
+              className='ion-ios-keypad'
+              style={{ fontSize: '1.5rem', marginRight: '0.5rem' }} />
+            Play a practice game now
+          </NavLink>
+        </div>
       </div>
     );
   }
