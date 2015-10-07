a78676d1ae261f833ab6ef4f1bf39ababd69cef1
meriadec <meriadec.pillet@gmail.com>
2015-10-07 18:50:41 +0200

    feat(languages): work on language page, but i dont like it

--- components/pages/Languages.js
+++ components/pages/Languages.js
@@ -5,9 +5,30 @@ import React from 'react';
 export default class LanguagesPage extends React.Component {
 
   render () {
+
+    const languages = [
+      'javascript',
+      'python',
+      'ruby',
+      'css',
+      'html',
+      'c++',
+      'c'
+    ];
+
     return (
-      <div>
-        here is a list of languages
+      <div className='LanguagesList'>
+        {languages.map((l, i) => (
+          <div
+            key={i}
+            className='LanguageItem'>
+            <div className='img'>
+              <img
+                src={`assets/images/languages/${l}.svg`} />
+            </div>
+            <span>{l}</span>
+          </div>
+        ))}
       </div>
     );
   }
