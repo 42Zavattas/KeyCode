65c235cece667d6c2445b46166ce05c76e998ba6
meriadec <meriadec.pillet@gmail.com>
2015-10-08 16:18:46 +0200

    fix(props-differ): little fix

--- api/services/utils.service.js
+++ api/services/utils.service.js
@@ -4,7 +4,7 @@ export function propsDiffer (one, two, arr) {
   if (!arr) { arr = Object.keys(two); }
   const len = arr.length;
   for (let i = 0; i < len; ++i) {
-    if (one[i] !== two[i]) { return true; }
+    if (one[arr[i]] !== two[arr[i]]) { return true; }
   }
   return false;
 }
