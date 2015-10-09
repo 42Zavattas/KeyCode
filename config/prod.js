d87e338e0486a06ec1d5d70b8e55f0a4f40cc98a
meriadec <meriadec.pillet@gmail.com>
2015-10-09 19:47:19 +0200

    chore(url): update api url for prod

--- config/prod.js
+++ config/prod.js
@@ -4,7 +4,7 @@ export default {
 
   postgres: process.env.POSTGRES_PROD,
 
-  apiUrl: 'http://localhost:3000/api' || 'http://keycode.sh/api',
+  apiUrl: 'http://keycode.fugitive.link/api',
   callbackURL: 'http://keycode.sh/api/auth/callback'
 
 };
