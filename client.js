dcaaa10126fb8e122cc3f78f541d91abade1f38b
meriadec <meriadec.pillet@gmail.com>
2015-10-09 17:48:17 +0200

    feat(debug): add some debugging stuff

--- client.js
+++ client.js
@@ -1,14 +1,19 @@
 'use strict';
 
 import React from 'react';
+import debug from 'debug';
 import { createElementWithContext } from 'fluxible-addons-react';
+
+const debugClient = debug('keycode');
 
 import app from './app';
 
 const dehydratedState = window.App;
 
 window.React = React;
+window.fluxibleDebug = debug;
 
+debugClient('rehydrating app');
 app.rehydrate(dehydratedState, (err, context) => {
 
   if (err) { throw err; }
@@ -16,6 +21,8 @@ app.rehydrate(dehydratedState, (err, context) => {
   window.context = context;
   const mountNode = document.getElementById('app');
 
-  React.render(createElementWithContext(context), mountNode);
+  React.render(createElementWithContext(context), mountNode, () => {
+    debugClient('react rendered');
+  });
 
 });
